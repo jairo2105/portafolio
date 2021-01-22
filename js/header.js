b00d528f749ca1ca70c1b406563ae8b4b9341d37
jairo2105 <69696822+jairo2105@users.noreply.github.com>
2021-01-21 21:47:50 -0600

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,23 @@
+var btn = document.getElementById("btnMenu");
+var barras = document.getElementsByClassName("bar");
+var lista = document.getElementById("op");
+var logo = document.getElementsByClassName("anim");
+
+btn.addEventListener("click",btnAnimacionStart);
+function btnAnimacionStart(){
+    lista.style.marginLeft="0";
+    barras[0].style.rotate="-45deg"
+    barras[1].style.width="0px"
+    barras[2].style.rotate="45deg"
+    btn.removeEventListener("click",btnAnimacionStart);
+    btn.addEventListener("click",btnAnimacionEnd);
+}
+function btnAnimacionEnd(){
+    lista.style.marginLeft="-100%";
+    barras[0].style.rotate="0deg"
+    barras[1].style.width="40px"
+    barras[2].style.rotate="0deg"
+    btn.removeEventListener("click",btnAnimacionEnd);
+    btn.addEventListener("click",btnAnimacionStart);
+}
+
